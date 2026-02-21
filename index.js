02af82aaa0041f4c2bc22ec7fe92dba83764fa55
mmari13 <mmatous1@kent.edu>
2026-02-21 00:38:59 -0500

    Hello from render updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js on Render!");
+  res.send("Hello! This is Marianna's Web Proggramming II app - Version 2");
 });
 
 app.listen(PORT, () => {
